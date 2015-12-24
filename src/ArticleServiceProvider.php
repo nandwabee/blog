2b55ff4988f4af6ca29bbc3f1900e63be1704b2a
Bernard Nandwa <nandwabee@gmail.com>
2015-12-24 21:45:58 +0300

    Flesh out the service provider.The config options need to be completed later on

--- src/ArticleServiceProvider.php
+++ src/ArticleServiceProvider.php
@@ -5,11 +5,41 @@ namespace Investigatively\Article;
 use Illuminate\Support\ServiceProvider;
 
 class ArticleServiceProvider extends ServiceProvider{
+  /**
+   * Boot method for the service provider
+   */
   public function boot(){
+    /**
+     * Load custom views for the package.
+     */
+    $this->loadViewsFrom(__DIR__.'/Views', 'investigatively');
 
+    /**
+     * Publish the package assets
+     */
+    $this->publishes([
+        __DIR__.'/Views' => base_path('resources/views/vendor/investigatively'),
+        __DIR__.'/Config/investigatively.php' => config_path('investigatively.php'),
+    ]);
+
+
+    /**
+     * Register the routes file
+     */
+    if (! $this->app->routesAreCached()) {
+      require __DIR__.'/Config/routes.php';
+    }
   }
 
+  /**
+   * Register method for the package.
+   */
   public function register(){
-    
+    /**
+     * Merge config with what the user has provided.
+     */
+    $this->mergeConfigFrom(
+        __DIR__.'/Config/courier.php', 'investigatively'
+    );
   }
 }
